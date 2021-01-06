64764957a8aa2a4374c3011fb5782bf18f6e94fc
Laszlo Daru <daruu@hotmail.com>
2021-01-06 16:01:25 +0100

    feat(Error handling & Posts route/controller)

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ app.get('/', (req, res) => {
 	res.send('Helloo there!');
 });
 
+app.use((req, res, next) => {
+	const error = new Error('Not found');
+	error.status = 404;
+	next(error);
+});
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	res.json({
+		error: {
+			message: error.message,
+		},
+	});
+});
+
 try {
 	mongoose.connect(
 		process.env.DB_SERVER,
--- routes/blogposts.js
+++ routes/blogposts.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.send('Posts here!!!');
+router.get('/', (req, res, next) => {
+	res.status(200).json({
+		message: 'GET request Post page',
+	});
+});
+
+router.post('/', (req, res, next) => {
+	res.status(200).json({
+		message: 'POST request Post page',
+	});
+});
+
+router.get('/:postId', (req, res, next) => {
+	const id = rew.param.postId;
+});
+
+router.patch('/:postId', (req, res, next) => {
+	res.status(200).json({
+		message: 'Post was updated successfully!',
+	});
+});
+router.delete('/:postId', (req, res, next) => {
+	res.status(200).json({
+		message: 'Post was Deleted!',
+	});
 });
 
 module.exports = router;
