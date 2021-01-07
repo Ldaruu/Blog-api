65295e1e8e776dc5d17e271e80cfb048edbb9c99
Laszlo Daru <daruu@hotmail.com>
2021-01-07 12:10:55 +0100

    refact(Image uploader)

--- routes/blogposts.js
+++ routes/blogposts.js
@@ -3,39 +3,15 @@ const router = express.Router();
 const multer = require('multer');
 const withAuth = require('../middleware/withAuth');
 const BlogPostsController = require('../controllers/blogPosts');
+const fileUploads = require('../middleware/fileUploads');
 require('dotenv/config');
-
-const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, './uploads/');
-	},
-	filename: function (req, file, cb) {
-		cb(null, new Date().toISOString() + file.originalname);
-	},
-});
-
-const fileFilter = (req, file, cb) => {
-	if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png ') {
-		cb(null, true);
-	} else {
-		cb(null, false);
-	}
-};
-
-const upload = multer({
-	storage: storage,
-	limits: {
-		fileSize: 1024 * 1024 * 5,
-	},
-	fileFilter: fileFilter,
-});
 
 router.get('/', BlogPostsController.blogPosts_get_all);
 
 router.post(
 	'/',
 	withAuth,
-	upload.single('postImage'),
+	fileUploads.imageUploads,
 	BlogPostsController.blogPost_create
 );
 
