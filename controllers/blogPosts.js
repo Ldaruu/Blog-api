c785123b2b3bd3bdb57ef55faceb2231ef59e75b
Laszlo Daru <daruu@hotmail.com>
2021-01-07 12:00:27 +0100

    feat(refactor blogPost routes to controlles)

---
+++ controllers/blogPosts.js
@@ -0,0 +1,110 @@
+const BlogPost = require('../models/blogPost');
+const mongoose = require('mongoose');
+
+exports.blogPosts_get_all = (req, res, next) => {
+	BlogPost.find()
+		.select('_id title content postImage')
+		.exec()
+		.then((data) => {
+			const response = {
+				count: data.length,
+				posts: data.map((d) => {
+					return {
+						id: d._id,
+						title: d.title,
+						content: d.content,
+						postImage: d.postImage,
+						request: {
+							type: 'GET',
+							url: process.env.API_URL + '/posts/' + d._id,
+						},
+					};
+				}),
+			};
+			res.status(200).json(response);
+		})
+		.catch((err) => {
+			res.status(500).json({ error: err });
+		});
+};
+
+exports.blogPost_create = (req, res, next) => {
+	const blogPost = new BlogPost({
+		_id: new mongoose.Types.ObjectId(),
+		title: req.body.title,
+		content: req.body.content,
+		postImage: req.file.path,
+	});
+	blogPost
+		.save()
+		.then((result) => {
+			res.status(201).json({
+				message: 'Post was created!',
+				post: {
+					id: result._id,
+					title: result.title,
+					content: result.content,
+					request: {
+						type: 'GET',
+						url: process.env.API_URL + '/posts/' + result._id,
+					},
+				},
+			});
+		})
+		.catch((err) => {
+			res.status(500).json({
+				error: err,
+			});
+		});
+};
+
+exports.blogPosts_get_post = (req, res, next) => {
+	const id = req.params.postId;
+	BlogPost.findById(id)
+		.select('_id title content postImage')
+		.exec()
+		.then((data) => {
+			if (data) {
+				res.status(200).json(data);
+			} else {
+				res.status(404).json({ message: 'Post does not exist!' });
+			}
+		})
+		.catch((err) => {
+			res.status(500).json({ error: err });
+		});
+};
+
+exports.blogPosts_update_post = (req, res, next) => {
+	const id = req.params.postId;
+	const updateOps = {};
+	for (const ops of req.body) {
+		updateOps[ops.propName] = ops.value;
+	}
+	BlogPost.updateOne({ _id: id }, { $set: updateOps })
+		.exec()
+		.then((result) => {
+			res.status(200).json({
+				message: 'Post was updated!',
+				request: {
+					type: 'GET',
+					url: process.env.API_URL + '/posts/' + id,
+				},
+			});
+		})
+		.catch((err) => {
+			res.status(500).json(err);
+		});
+};
+
+exports.blogPosts_delete_post = (req, res, next) => {
+	const id = req.params.postId;
+	BlogPost.deleteOne({ _id: id })
+		.exec()
+		.then((result) => {
+			res.status(200).json({ message: 'Post deleted!' });
+		})
+		.catch((err) => {
+			res.status(500).json({ error: err });
+		});
+};
--- routes/blogposts.js
+++ routes/blogposts.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 const multer = require('multer');
 const withAuth = require('../middleware/withAuth');
+const BlogPostsController = require('../controllers/blogPosts');
 require('dotenv/config');
 
 const storage = multer.diskStorage({
@@ -30,114 +30,19 @@ const upload = multer({
 	fileFilter: fileFilter,
 });
 
-const BlogPost = require('../models/blogPost');
+router.get('/', BlogPostsController.blogPosts_get_all);
 
-router.get('/', (req, res, next) => {
-	BlogPost.find()
-		.select('_id title content postImage')
-		.exec()
-		.then((data) => {
-			const response = {
-				count: data.length,
-				posts: data.map((d) => {
-					return {
-						id: d._id,
-						title: d.title,
-						content: d.content,
-						postImage: d.postImage,
-						request: {
-							type: 'GET',
-							url: process.env.API_URL + '/posts/' + d._id,
-						},
-					};
-				}),
-			};
-			res.status(200).json(response);
-		})
-		.catch((err) => {
-			res.status(500).json({ error: err });
-		});
-});
+router.post(
+	'/',
+	withAuth,
+	upload.single('postImage'),
+	BlogPostsController.blogPost_create
+);
 
-router.post('/', withAuth, upload.single('postImage'), (req, res, next) => {
-	const blogPost = new BlogPost({
-		_id: new mongoose.Types.ObjectId(),
-		title: req.body.title,
-		content: req.body.content,
-		postImage: req.file.path,
-	});
-	blogPost
-		.save()
-		.then((result) => {
-			res.status(201).json({
-				message: 'Post was created!',
-				post: {
-					id: result._id,
-					title: result.title,
-					content: result.content,
-					request: {
-						type: 'GET',
-						url: process.env.API_URL + '/posts/' + result._id,
-					},
-				},
-			});
-		})
-		.catch((err) => {
-			res.status(500).json({
-				error: err,
-			});
-		});
-});
+router.get('/:postId', BlogPostsController.blogPosts_get_post);
 
-router.get('/:postId', (req, res, next) => {
-	const id = req.params.postId;
-	BlogPost.findById(id)
-		.select('_id title content postImage')
-		.exec()
-		.then((data) => {
-			if (data) {
-				res.status(200).json(data);
-			} else {
-				res.status(404).json({ message: 'Post does not exist!' });
-			}
-		})
-		.catch((err) => {
-			res.status(500).json({ error: err });
-		});
-});
+router.patch('/:postId', withAuth, BlogPostsController.blogPosts_update_post);
 
-router.patch('/:postId', withAuth, (req, res, next) => {
-	const id = req.params.postId;
-	const updateOps = {};
-	for (const ops of req.body) {
-		updateOps[ops.propName] = ops.value;
-	}
-	BlogPost.updateOne({ _id: id }, { $set: updateOps })
-		.exec()
-		.then((result) => {
-			res.status(200).json({
-				message: 'Post was updated!',
-				request: {
-					type: 'GET',
-					url: process.env.API_URL + '/posts/' + id,
-				},
-			});
-		})
-		.catch((err) => {
-			res.status(500).json(err);
-		});
-});
-
-router.delete('/:postId', withAuth, (req, res, next) => {
-	const id = req.params.postId;
-	BlogPost.deleteOne({ _id: id })
-		.exec()
-		.then((result) => {
-			res.status(200).json({ message: 'Post deleted!' });
-		})
-		.catch((err) => {
-			res.status(500).json({ error: err });
-		});
-});
+router.delete('/:postId', withAuth, BlogPostsController.blogPosts_delete_post);
 
 module.exports = router;
